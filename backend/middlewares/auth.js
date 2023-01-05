931626c9ca08a402f49ba052885052307f862749
Omer Dotchamou <ddaniomer@gmail.com>
2023-01-05 18:46:15 +0100

    création du middleware d'authentification

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,20 @@
+// Import de la dépendance jsonwebtoken
+const jwt = require('jsonwebtoken')
+
+// Requête d'authentification de l'utilisateur
+module.exports = (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(' ')[1]
+        // Vérifier le token avec la méthode verify de jwt
+        const decodedToken = jwt.verify(token, 'TERCES_NEKOT_MODNAR')
+        // Récupérer l'id utilisateur
+        const userId = decodedToken.userId
+        // Ajouter l'id user à l'objet request
+        req.auth = {
+            userId : userId 
+        }
+        next()
+    }catch(error) {
+        res.status(401).json({ error })
+    }
+}
