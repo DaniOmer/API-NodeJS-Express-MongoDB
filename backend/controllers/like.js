2d8732366c6de3bf2aa06f893910d819072de50c
Omer Dotchamou <ddaniomer@gmail.com>
2023-01-19 03:49:14 +0100

    Logique métier pour les likes + la route

---
+++ backend/controllers/like.js
@@ -0,0 +1,49 @@
+const Sauce = require('../models/sauces') // sauceSchema
+
+// un user peut like, dislike, ou annuler
+exports.likeStatus = (req, res) => {
+	const like = req.body.like
+	const userId = req.body.userId
+
+	// on cherche la sauce sélectionnée
+	Sauce.findOne({ _id: req.params.id })
+		.then((sauce) => {
+			// find : vérifie si userId existe déjà
+			let userLike = sauce.usersLiked.find((id) => id === userId)
+			let userDislike = sauce.usersDisliked.find((id) => id === userId)
+
+			// Switch case pour gérer +1, 0 ou -1 like
+			switch (like) {
+				// like +1
+				case 1:
+					sauce.likes += 1
+					sauce.usersLiked.push(userId)
+					break
+
+				// annule -1
+				case 0:
+					if (userLike) {
+						sauce.likes -= 1
+						sauce.usersLiked = sauce.usersLiked.filter((id) => id !== userId)
+					}
+					if (userDislike) {
+						sauce.dislikes -= 1
+						sauce.usersDisliked = sauce.usersDisliked.filter(
+							(id) => id !== userId
+						)
+					}
+					break
+
+				// dislike +1
+				case -1:
+					sauce.dislikes += 1
+					sauce.usersDisliked.push(userId)
+			}
+			// sauvegarde la sauce
+			sauce
+				.save()
+				.then(() => res.status(201).json({ message: 'save sauce' }))
+				.catch((error) => res.status(400).json({ error }))
+		})
+		.catch((error) => res.status(500).json({ error }))
+}
