e89c93c0a7579cf07e0a96d5d6ca23dcb86c51d9
Omer Dotchamou <ddaniomer@gmail.com>
2023-01-19 03:51:38 +0100

    Update du middleware d'authentification

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
     try{
         const token = req.headers.authorization.split(' ')[1]
         // Vérifier le token avec la méthode verify de jwt
-        const decodedToken = jwt.verify(token, 'TERCES_NEKOT_MODNAR')
+        const decodedToken = jwt.verify(token, process.env.TOKEN_CRYPT)
         // Récupérer l'id utilisateur
         const userId = decodedToken.userId
         // Ajouter l'id user à l'objet request
